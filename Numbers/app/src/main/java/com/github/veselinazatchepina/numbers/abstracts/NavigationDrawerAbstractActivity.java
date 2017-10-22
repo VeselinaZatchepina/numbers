5a1ddef8a9f619c81381cee31f0cdd7c41ddac6b
Veselina Zatchepina <techpro20@mail.ru>
2017-10-22 23:56:33 +0300

    feat: add abstract activity

---
+++ Numbers/app/src/main/java/com/github/veselinazatchepina/numbers/abstracts/NavigationDrawerAbstractActivity.java
@@ -0,0 +1,124 @@
+package com.github.veselinazatchepina.numbers.abstracts;
+
+
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.text.SpannableString;
+import android.text.style.TextAppearanceSpan;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.github.veselinazatchepina.numbers.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class NavigationDrawerAbstractActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    @BindView(R.id.drawer_layout)
+    DrawerLayout mDrawerLayout;
+    @BindView(R.id.navigation_view)
+    NavigationView mNavigationView;
+    @BindView(R.id.toolbar)
+    Toolbar mToolbar;
+    @BindView(R.id.fab)
+    FloatingActionButton mFloatingActionButton;
+
+    int mFabImageResourceId = setFabImageResourceId();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_navigation_drawer);
+        ButterKnife.bind(this);
+        defineNavigationDrawer();
+        defineFab();
+    }
+
+    private void defineNavigationDrawer() {
+        setSupportActionBar(mToolbar);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, mDrawerLayout, mToolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        mDrawerLayout.setDrawerListener(toggle);
+        toggle.syncState();
+        Menu menu = mNavigationView.getMenu();
+        MenuItem tools = menu.findItem(R.id.other);
+        SpannableString s = new SpannableString(tools.getTitle());
+        s.setSpan(new TextAppearanceSpan(this, R.style.NavigationViewStyle), 0, s.length(), 0);
+        tools.setTitle(s);
+        mNavigationView.setNavigationItemSelectedListener(this);
+    }
+
+    public void defineFab() {
+        setFabBackgroundImage(mFloatingActionButton, mFabImageResourceId);
+        mFloatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                defineActionWhenFabIsPressed();
+            }
+        });
+    }
+
+    public int setFabImageResourceId() {
+        return R.drawable.ic_add_white_24dp;
+    }
+
+    public void setFabBackgroundImage(FloatingActionButton fab, int imageResourceId) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            fab.setImageDrawable(getResources().getDrawable(imageResourceId, getTheme()));
+        } else {
+            fab.setImageDrawable(getResources().getDrawable(imageResourceId));
+        }
+    }
+
+    public void defineActionWhenFabIsPressed() {
+        Intent intent = null;
+        startActivity(intent);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (mDrawerLayout.isDrawerOpen(GravityCompat.START)) {
+            mDrawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        Intent intent = null;
+        switch (item.getItemId()) {
+            case R.id.menu_numbers:
+                intent = null;
+                break;
+            case R.id.menu_saved_numbers:
+                intent = null;
+                break;
+            case R.id.menu_history:
+                intent = null;
+                break;
+            case R.id.menu_about:
+                intent = null;
+                break;
+        }
+        if (intent != null) {
+            startActivity(intent);
+        }
+        return true;
+    }
+}
---
+++ Numbers/app/src/main/java/com/github/veselinazatchepina/numbers/numbers/NumbersActivity.java
@@ -0,0 +1,8 @@
+package com.github.veselinazatchepina.numbers.numbers;
+
+import com.github.veselinazatchepina.numbers.abstracts.NavigationDrawerAbstractActivity;
+
+public class NumbersActivity extends NavigationDrawerAbstractActivity {
+
+
+}
